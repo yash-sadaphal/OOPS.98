51fbba488f3b259b7f873d0c51e706dff6c86d1d
Yash-Sadaphal <sadaphalyash87@gmail.com>
2025-07-19 19:18:43 +0530

    2nd commit

---
+++ 1st.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class Student{
+    int rollno;
+    string name;
+    public:
+     void accept();
+     void display();
+}S;
+void Student::accept() 
+{
+    cout<<"Enter the roll no: ";
+    cin>>rollno;
+    cout<<"Enter the name: ";
+    cin>>name;
+}
+void Student::display()
+{
+    cout<<"\nName is :"<<name<<"\n"<<"Rollno of " <<name<<" is : "<<rollno;
+}
+int main()
+{
+    S.accept();
+    S.display();
+    return 0;
+}
